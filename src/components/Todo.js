1351b26840b24260acb63263820d5b4f8faebf1e
GitHubDavidTorres <david.torres.santander@gmail.com>
2021-12-10 20:42:52 -0300

    master

---
+++ src/components/Todo.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
+function Todo(props) {
+  const [showModal, setShowModal] = useState(false);
+
+  function deleteHandler() {
+    setShowModal(true);
+  }
+
+  function closeModalHandler() {
+    setShowModal(false);
+  }
+
+  return (
+    <div className="card">
+      <h2>{props.text}</h2>
+      <div className="actions">
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
+      </div>
+      {showModal ? (
+        <Modal
+          onCancel={closeModalHandler}
+          onConfirm={closeModalHandler}
+        ></Modal>
+      ) : null}
+      {showModal && <Backdrop onClick={closeModalHandler}></Backdrop>}
+    </div>
+  );
+}
+
+export default Todo;
